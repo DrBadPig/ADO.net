95b860ff47d17e07d853b50f6b14f1c16365cab6
DrBadPig <stuartkirk14@gmail.com>
2020-09-11 19:50:24 +0300

    first commit

---
+++ ADO NET 1/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace ADO_NET_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.comboSearchType = new System.Windows.Forms.ComboBox();
+            this.textBoxTableName = new System.Windows.Forms.TextBox();
+            this.labelTableName = new System.Windows.Forms.Label();
+            this.buttonTableHelp = new System.Windows.Forms.Button();
+            this.buttonFind = new System.Windows.Forms.Button();
+            this.buttonSetPath = new System.Windows.Forms.Button();
+            this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // comboSearchType
+            // 
+            this.comboSearchType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboSearchType.FormattingEnabled = true;
+            this.comboSearchType.Items.AddRange(new object[] {
+            "Verbs",
+            "Adjectives"});
+            this.comboSearchType.Location = new System.Drawing.Point(12, 42);
+            this.comboSearchType.Name = "comboSearchType";
+            this.comboSearchType.Size = new System.Drawing.Size(180, 21);
+            this.comboSearchType.TabIndex = 0;
+            // 
+            // textBoxTableName
+            // 
+            this.textBoxTableName.Location = new System.Drawing.Point(12, 82);
+            this.textBoxTableName.MaxLength = 200;
+            this.textBoxTableName.Name = "textBoxTableName";
+            this.textBoxTableName.Size = new System.Drawing.Size(154, 20);
+            this.textBoxTableName.TabIndex = 1;
+            // 
+            // labelTableName
+            // 
+            this.labelTableName.AutoSize = true;
+            this.labelTableName.BackColor = System.Drawing.Color.Transparent;
+            this.labelTableName.ForeColor = System.Drawing.SystemColors.Control;
+            this.labelTableName.Location = new System.Drawing.Point(10, 66);
+            this.labelTableName.Name = "labelTableName";
+            this.labelTableName.Size = new System.Drawing.Size(63, 13);
+            this.labelTableName.TabIndex = 2;
+            this.labelTableName.Text = "Table name";
+            // 
+            // buttonTableHelp
+            // 
+            this.buttonTableHelp.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonTableHelp.BackgroundImage")));
+            this.buttonTableHelp.Cursor = System.Windows.Forms.Cursors.Help;
+            this.buttonTableHelp.Location = new System.Drawing.Point(172, 82);
+            this.buttonTableHelp.Name = "buttonTableHelp";
+            this.buttonTableHelp.Size = new System.Drawing.Size(20, 20);
+            this.buttonTableHelp.TabIndex = 3;
+            this.buttonTableHelp.Text = "?";
+            this.buttonTableHelp.UseVisualStyleBackColor = true;
+            this.buttonTableHelp.Click += new System.EventHandler(this.buttonTableHelp_Click);
+            // 
+            // buttonFind
+            // 
+            this.buttonFind.BackColor = System.Drawing.Color.Transparent;
+            this.buttonFind.Location = new System.Drawing.Point(198, 13);
+            this.buttonFind.Name = "buttonFind";
+            this.buttonFind.Size = new System.Drawing.Size(62, 89);
+            this.buttonFind.TabIndex = 4;
+            this.buttonFind.Text = "Find";
+            this.buttonFind.UseVisualStyleBackColor = false;
+            this.buttonFind.Click += new System.EventHandler(this.buttonFind_Click);
+            // 
+            // buttonSetPath
+            // 
+            this.buttonSetPath.Location = new System.Drawing.Point(12, 13);
+            this.buttonSetPath.Name = "buttonSetPath";
+            this.buttonSetPath.Size = new System.Drawing.Size(180, 23);
+            this.buttonSetPath.TabIndex = 5;
+            this.buttonSetPath.Text = "Set Path";
+            this.buttonSetPath.UseVisualStyleBackColor = true;
+            this.buttonSetPath.Click += new System.EventHandler(this.buttonSetPath_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(273, 116);
+            this.Controls.Add(this.buttonSetPath);
+            this.Controls.Add(this.buttonFind);
+            this.Controls.Add(this.buttonTableHelp);
+            this.Controls.Add(this.labelTableName);
+            this.Controls.Add(this.textBoxTableName);
+            this.Controls.Add(this.comboSearchType);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboSearchType;
+        private System.Windows.Forms.TextBox textBoxTableName;
+        private System.Windows.Forms.Label labelTableName;
+        private System.Windows.Forms.Button buttonTableHelp;
+        private System.Windows.Forms.Button buttonFind;
+        private System.Windows.Forms.Button buttonSetPath;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog;
+    }
+}
+
---
+++ ADO NET 1/Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ADO_NET_1
+{
+    public partial class Form1 : Form
+    {
+        string path;
+
+        Dictionary<string, int> verbs = new Dictionary<string, int>();
+        Dictionary<string, int> adjs = new Dictionary<string, int>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonTableHelp_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("It will be created new table in your database. " +
+                "If table with name you chose already exists, it will be deleted. Sucks",
+                "Hey you, wait a minute", MessageBoxButtons.OK);
+        }
+
+        private void buttonSetPath_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                path = folderBrowserDialog.SelectedPath;
+            }
+        }
+        /// <summary>
+        /// Finds all words by type from all txt files
+        /// </summary>
+        /// <param name="path">path to txt file</param>
+        /// <param name="type">"v" is for verbs and "a" is for adjectives</param>
+        private void CheckAllFiles(string path, string type)
+        {
+            DirectoryInfo dinfo = new DirectoryInfo(path);
+
+            if (dinfo.Exists)
+            {
+                try
+                {
+                    FileInfo[] _files = dinfo.GetFiles();
+                    foreach (FileInfo current in _files)
+                    {
+                        if (current.Extension == ".txt")
+                        {
+                            switch (type)
+                            {
+                                case "v":
+                                    GetVerbs(current.FullName);
+                                    break;
+                                case "a":
+                                    GetAdj(current.FullName);
+                                    break;
+                                default:
+                                    break;
+                            }
+                        }
+                    }
+
+                    DirectoryInfo[] dirs = dinfo.GetDirectories();
+                    foreach (DirectoryInfo current in dirs)
+                    {
+                        CheckAllFiles(path + @"\" + current.Name, type);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Path is not exists");
+            }
+        }
+
+        private void GetVerbs(string path)
+        {
+            StreamReader reader = new StreamReader(path);
+
+            string text = reader.ReadToEnd();
+
+            foreach (var item in text.Split('.', ' ', '?', '!', ',', '\r', '\n', '\t'))
+            {
+                if (item.Length > 3)
+                {
+                    if ((item[item.Length - 1] == 'ь' && item[item.Length - 2] == 'т') ||
+                        (item[item.Length - 1] == 'и' && item[item.Length - 2] == 'т'))
+                    {
+                        if (verbs.ContainsKey(item))
+                        {
+                            verbs[item]++;
+                        }
+                        else
+                        {
+                            verbs.Add(item, 1);
+                        }
+                    }
+                }
+            }
+
+            reader.Close();
+        }
+
+        private void GetAdj(string path)
+        {
+            StreamReader reader = new StreamReader(path);
+
+            string text = reader.ReadToEnd();
+
+            foreach (var item in text.Split('.', ' ', '?', '!', ',', '\r', '\n', '\t'))
+            {
+                if (item.Length > 3)
+                {
+                    if ((item[item.Length - 1] == 'й' && item[item.Length - 2] == 'ы') 
+                    ||
+                    (item[item.Length - 1] == 'й' && item[item.Length - 2] == 'и')
+                    ||
+                    (item[item.Length - 1] == 'я' && item[item.Length - 2] == 'а')
+                    ||
+                    (item[item.Length - 1] == 'я' && item[item.Length - 2] == 'я')
+                    ||
+                    (item[item.Length - 1] == 'е' && item[item.Length - 2] == 'о')
+                    ||
+                    (item[item.Length - 1] == 'е' && item[item.Length - 2] == 'е')
+                    ||
+                    (item[item.Length - 1] == 'е' && item[item.Length - 2] == 'ы')
+                    ||
+                    (item[item.Length - 1] == 'е' && item[item.Length - 2] == 'и')
+                    ||
+                    (item[item.Length - 1] == 'х' && item[item.Length - 2] == 'и')
+                    ||
+                    (item[item.Length - 1] == 'х' && item[item.Length - 2] == 'ы')
+                    ||
+                    (item[item.Length - 1] == 'й' && item[item.Length - 2] == 'е')
+                    ||
+                    (item[item.Length - 1] == 'й' && item[item.Length - 2] == 'о')
+                    ||
+                    (item[item.Length - 1] == 'м' && item[item.Length - 2] == 'ы')
+                    ||
+                    (item[item.Length - 1] == 'м' && item[item.Length - 2] == 'и')
+                    )
+                    {
+                        if (verbs.ContainsKey(item))
+                        {
+                            adjs[item]++;
+                        }
+                        else
+                        {
+                            adjs.Add(item, 1);
+                        }
+                    }
+                }
+            }
+            reader.Close();
+        }
+
+        private void buttonFind_Click(object sender, EventArgs e)
+        {
+            if (textBoxTableName.Text.Length > 0 && comboSearchType.SelectedItem != null && path != null)
+            {
+                switch (comboSearchType.SelectedItem.ToString())
+                {
+                    case "Verbs":
+                        CheckAllFiles(path, "v");
+                        break;
+                    case "Adjectives":
+                        CheckAllFiles(path, "a");
+                        break;
+                }
+            }
+        }
+    }
+}
